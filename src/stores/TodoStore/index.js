bd855a5c902c927a5909b585a3224bb796d1291a
ib_admin <sirivalli139@gmail.com>
2019-08-08 09:50:00 +0530

    after refactoring of addTodo

--- src/stores/TodoStore/index.js
+++ src/stores/TodoStore/index.js
@@ -1,7 +1,14 @@
+import { observable } from "mobx";
+
 class TodoStore {
-  todos = [];
+  count = 0;
+  @observable todos = [];
   addTodo(description) {
-    this.todos.push(description);
+    const todo = {
+      description: description,
+      id: this.count++
+    };
+    this.todos.push(todo);
   }
 }
 export default TodoStore;
