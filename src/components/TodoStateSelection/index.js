3eb5c5a3f27a749967d45f2dc386881dbb91d005
ib_admin <sirivalli139@gmail.com>
2019-08-09 13:18:40 +0530

    after completion of green state

--- src/components/TodoStateSelection/index.js
+++ src/components/TodoStateSelection/index.js
@@ -1,7 +1,21 @@
 import React, { Component } from "react";
 
 export default class TodoStateSelection extends Component {
+  submit = e => {
+    this.props.onSelection(e.target.value);
+  };
+
   render() {
-    return <div />;
+    return (
+      <div>
+        <button
+          value="completed"
+          onClick={this.submit}
+          data-testid="completed"
+        />
+        <button value="all" onClick={this.submit} data-testid="all" />
+        <button value="active" onClick={this.submit} data-testid="active" />
+      </div>
+    );
   }
 }
