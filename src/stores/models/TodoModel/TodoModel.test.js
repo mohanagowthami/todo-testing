d0b9881d660a372b1f9e2077909c6dbd3fcbacd5
ib_admin <sirivalli139@gmail.com>
2019-08-09 10:30:35 +0530

    after code refactoring of todo model test cases of update todo description

--- src/stores/models/TodoModel/TodoModel.test.js
+++ src/stores/models/TodoModel/TodoModel.test.js
@@ -8,13 +8,11 @@ describe(" testing the todo model", () => {
     expect(todoStore.todos[0].isCompleted).toBe(true);
   });
 
-  it("it should test todomodel description", () => {
+  it("should test todomodel description updated or not", () => {
+    const todo = "wheter todo is updated or not";
     const todoStore = new TodoStore();
     todoStore.addTodo(" be focused");
-    todoStore.todos[0].updateTodoDescription("wheter todo is updated or not");
-    expect(todoStore.todos[0].description).toBe(
-      "wheter todo is updated or not"
-    );
-    console.log(todoStore.todos[0].description);
+    todoStore.todos[0].updateTodoDescription(todo);
+    expect(todoStore.todos[0].description).toBe(todo);
   });
 });
