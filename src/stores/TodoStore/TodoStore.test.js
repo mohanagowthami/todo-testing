cc72e75bc1f28c4f6a33c6864395fa0ffe814005
ib_admin <sirivalli139@gmail.com>
2019-08-09 11:17:58 +0530

    red state of todos selected state

--- src/stores/TodoStore/TodoStore.test.js
+++ src/stores/TodoStore/TodoStore.test.js
@@ -1,8 +1,19 @@
 import TodoStore from "./index";
-const todoStore = new TodoStore();
 describe(" todostore test suit", () => {
   it(" should test add todo function", () => {
+    const todoStore = new TodoStore();
     todoStore.addTodo("complete todo tdd task throughly");
     expect(todoStore.todos.length).toBe(1);
+  });
+
+  it(" should test the selected state of todos whether they are selected or not", () => {
+    const todoStore = new TodoStore();
+    const selectedStateTodos = todoStore.todos.filter(todo => {
+      return todo.isCompleted === true;
+    });
+    updatedSelectedStateTodos = todoStore.updateSelectedTodoState("completed");
+    expected(JSON.stringify(selectedStateTodos)).toBe(
+      updatedSelectedStateTodos
+    );
   });
 });
