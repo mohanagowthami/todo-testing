f1c9375b965f76571afc7648f31f8deff667c98f
ib_admin <sirivalli139@gmail.com>
2019-08-09 14:19:55 +0530

    after using constants and changing child prop in render method

--- src/components/TodoStateSelection/TodoStateSelection.test.js
+++ src/components/TodoStateSelection/TodoStateSelection.test.js
@@ -1,12 +1,20 @@
 import TodoStateSelection from "./index";
 import React from "react";
 import { render, fireEvent, cleanup } from "@testing-library/react";
+import { all, active, completed } from "../constants.js";
+
 describe("testing the todo state selection component", () => {
   afterEach(cleanup);
   const myMock = jest.fn();
   it("should test call back function with argument of selection state", () => {
-    const { getByTestId } = render(<TodoStateSelection onSelection={myMock} />);
-    fireEvent.click(getByTestId("active"));
-    expect(myMock).toHaveBeenCalledWith("active");
+    const { getByTestId } = render(
+      <TodoStateSelection onStateUpdate={myMock} />
+    );
+    fireEvent.click(getByTestId(active));
+    expect(myMock).toHaveBeenCalledWith(active);
+    fireEvent.click(getByTestId(completed));
+    expect(myMock).toHaveBeenCalledWith(completed);
+    fireEvent.click(getByTestId(all));
+    expect(myMock).toHaveBeenCalledWith(all);
   });
 });
--- src/components/TodoStateSelection/index.js
+++ src/components/TodoStateSelection/index.js
@@ -1,20 +1,20 @@
 import React, { Component } from "react";
-
+import { all, active, completed } from "../constants.js";
 export default class TodoStateSelection extends Component {
   submit = e => {
-    this.props.onSelection(e.target.value);
+    this.props.onStateUpdate(e.target.value);
   };
 
   render() {
     return (
       <div>
         <button
-          value="completed"
+          value={completed}
           onClick={this.submit}
-          data-testid="completed"
+          data-testid={completed}
         />
-        <button value="all" onClick={this.submit} data-testid="all" />
-        <button value="active" onClick={this.submit} data-testid="active" />
+        <button value={all} onClick={this.submit} data-testid={all} />
+        <button value={active} onClick={this.submit} data-testid={active} />
       </div>
     );
   }
